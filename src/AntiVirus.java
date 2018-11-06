87362554f952d458aa9802e498a60ed161a117e3
uottawatristan123 <tleve056@uottawa.ca>
2018-11-06 11:14:34 -0500

    Initial commit

---
+++ src/AntiVirus.java
@@ -0,0 +1,66 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AntiVirus {
+
+	public static void main(String[] args) {
+		String replaceWith = "xxxxxxxx";
+		// pass the path to the file as a parameter 
+	    File file = 
+	      new File("C:\\Users\\Tristan\\Desktop\\VirusDefinitions.txt"); 
+	    Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	  
+		List<String> virusDefinitions = new LinkedList<String>();
+	    while (sc.hasNextLine()) 
+	      virusDefinitions.add(sc.nextLine());
+	    
+	    
+	    for(String virus : virusDefinitions)
+	    {
+	    	try {
+	    		try (Stream<Path> paths = Files.walk(Paths.get("C:\\Users\\Tristan\\Desktop\\SearchLab3"))) {
+	    		    paths
+	    		        .filter(Files::isRegularFile)
+	    		        .forEach(path -> {
+	    		        	try (Stream<String> lines = Files.lines(path)) {
+	 	    		    	   List<String> replaced = lines
+	 	    		    	       .map(line-> line.replaceAll(virus, replaceWith))
+	 	    		    	       .collect(Collectors.toList());
+	 	    		    	   Files.write(path, replaced);
+	 	    		    	} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+	    		        });	    		    
+	    		} 
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }    
+	     
+	}
+	
+	public static String readFileAsString(String fileName)throws Exception 
+    { 
+      String data = ""; 
+      data = new String(Files.readAllBytes(Paths.get(fileName))); 
+      return data; 
+    }
+
+}
